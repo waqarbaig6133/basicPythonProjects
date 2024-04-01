bdbec3cf76212ec4e3f3501eccd40a0df49dda18
Onii-Chan <140295418+waqarbaig6133@users.noreply.github.com>
2024-03-31 20:13:12 -0400

    Create RockPaperSciss.py

---
+++ RockPaperSciss.py
@@ -0,0 +1,46 @@
+#Rock paper scissors basic python code
+
+import random
+options = ['scissors', 'paper', 'rock']
+
+me = 0
+comp = 0
+
+#Game goes up to 5 points
+while me <5 and comp <5:
+    my_choice = input("Pick rock, paper or scissors: ")
+    comp_choice = random.choice(options)
+
+    winning_conditions = [
+        (my_choice == "rock" and comp_choice == "scissors"),
+        (my_choice == "paper" and comp_choice == "rock"),
+        (my_choice == "scissors" and comp_choice == "paper")
+        ]
+    losing_conditions = [
+        (comp_choice == "rock" and my_choice == "scissors"),
+        (comp_choice == "paper" and my_choice == "rock"),
+        (comp_choice == "scissors" and my_choice == "paper")
+        ]
+    for n in winning_conditions:
+        if n == True:
+            me+=1
+            print("You got a point!")
+            print(me)
+            print(comp)
+    for n in losing_conditions:
+        if n == True:
+            comp +=1
+            print("It got a point!")
+            print(me)
+            print(comp)
+
+if me == 5:
+    print("You won!")
+else:
+    print("You lose!")
+        
+            
+    
+    
+    
+
