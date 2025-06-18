07ca267248949b0652aeba1689a17e1f5e158fc3
Waqar Abbas Baig <140295418+waqarbaig6133@users.noreply.github.com>
2025-06-18 17:34:46 +0000

    Update PermutationAlgo.py

--- PermutationAlgo.py
+++ PermutationAlgo.py
@@ -11,18 +11,25 @@ def perm(x):
                 copy[numb],copy[letter[0]] = copy[letter[0]],copy[numb]
                 if ''.join(copy) not in collection:
                     collection.append(''.join(copy))
-            
+
         i+=1
         if i == len(thing):
             i = 0
 perm(thi)
-for x in collection:
-    perm(x)
+for x in collection: 
+    perm(x) 
 for x in collection:
     if x[::-1] not in collection:
-        collection.append(x[::-1])
+        collection.append(x[::-1]) 
 print(collection)
-if (factorial(len(thi))/factorial(len(thi)-len(set(thi))+1)) == len(collection):
+
+duplicates = [] 
+for x in set(thi):
+    duplicates.append(thi.count(x))
+i = 1
+for x in duplicates:
+    i*=factorial(x)
+if (factorial(len(thi))/i) == len(collection):
     print('Good')
 else:
     print('Not')
